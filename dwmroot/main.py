f316d66cb1c5859fe0186b4c2a080dc82b9ef2de
Simon Jagoe <simon@simonjagoe.com>
2014-09-14 15:33:52 +0100

    Use xsetroot to actually set the root window name

--- dwmroot/main.py
+++ dwmroot/main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals
 
 from datetime import datetime
+import subprocess
 import time
 
 from .battery import get_battery_percentage
@@ -9,6 +10,7 @@ from .battery import get_battery_percentage
 def main():
     while True:
         battery = get_battery_percentage()
-        now = datetime.now()
-        print(battery, now.strftime('%Y/%m/%d %H:%M:%S'), sep=' | ')
+        now = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        dwm_root = '{0} | {1}'.format(battery, now)
+        subprocess.call(['xsetroot', '-name', dwm_root])
         time.sleep(1)
