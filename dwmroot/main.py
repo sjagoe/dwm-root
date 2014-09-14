055fbe7f0b1206e595dd7d11fc6984a307e26740
Simon Jagoe <simon@simonjagoe.com>
2014-09-14 15:30:01 +0100

    Run continuously; format for dwm on a single line

--- dwmroot/main.py
+++ dwmroot/main.py
@@ -1,7 +1,14 @@
 from __future__ import print_function, unicode_literals
 
+from datetime import datetime
+import time
+
 from .battery import get_battery_percentage
 
 
 def main():
-    print(get_battery_percentage())
+    while True:
+        battery = get_battery_percentage()
+        now = datetime.now()
+        print(battery, now.strftime('%Y/%m/%d %H:%M:%S'), sep=' | ')
+        time.sleep(1)
